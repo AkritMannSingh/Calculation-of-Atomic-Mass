e874586f7d34ec95489ad449dc57a8b30c8ff333
Akrit Mann Singh <akritmannsingh@gmail.com>
2025-11-26 07:01:24 +0530

    Update atomic_mass.c

--- atomic_mass.c
+++ atomic_mass.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 int main(){
+printf("Hey! I. this section we calculate the atomic number of an Element\n");
+
+printf("Chemistry Exploration🧪⚗️..\n);
+
   int z;
   int n;
   printf("Enter the atomic number:");
@@ -7,4 +11,8 @@ int main(){
   printf("Enter the number of neutrons:");
   scanf("%d", & n);
   printf("The atomic mass is %d\n", z+n);
+
+printf("Ending of Chemistry Section");
+
+return 0;
 }
